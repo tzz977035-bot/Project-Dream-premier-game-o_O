7e6219563cfb3b6a9ed8736a129ac8c8dbb7a292
Farm56 <sstg3455@gmail.com>
2026-02-24 18:13:11 +0700

    เริ่มเตะ

--- Duel.cpp
+++ Duel.cpp
@@ -18,7 +18,10 @@ struct Team {
     string buffedPosition;    
     int formationBuffPercent; 
 };
-////// 2
+
+const int STAT_DIFF_THRESHOLD = 10;     
+const int STAT_DIFF_BONUS_PERCENT = 10;
+
 float getMidfieldAvg(const Team& t) {
     int totalStat = 0;
     int count = 0;
@@ -74,4 +77,59 @@ bool duel1v1(const Player& attacker, const Player& defender, const Team& attacki
          << defender.name << " " << (100 - attackerChance) << "%] -> Dice Roll: " << roll << endl;
     
     return (roll <= attackerChance);
+}
+
+void startMatch(Team homeTeam, Team awayTeam) {
+    cout << "==========================================\n";
+    cout << "MATCH KICK-OFF: " << homeTeam.teamName << " (Home) VS " << awayTeam.teamName << " (Away)\n";
+    cout << "==========================================\n";
+    
+    bool matchEnded = false;
+    
+    while (!matchEnded) {
+        cout << "\n--- [MIDFIELD BATTLE: MF vs MF] ---" << endl;
+        float homeMidAvg = getMidfieldAvg(homeTeam);
+        float awayMidAvg = getMidfieldAvg(awayTeam);
+        
+        Team* attackingTeam = nullptr;
+        Team* defendingTeam = nullptr;
+        
+        if (homeMidAvg >= awayMidAvg) {
+            cout << ">> " << homeTeam.teamName << " dominates the midfield! (Avg Stat: " << homeMidAvg << " vs " << awayMidAvg << ")" << endl;
+            attackingTeam = &homeTeam;
+            defendingTeam = &awayTeam;
+        } else {
+            cout << ">> " << awayTeam.teamName << " takes control of the midfield! (Avg Stat: " << awayMidAvg << " vs " << homeMidAvg << ")" << endl;
+            attackingTeam = &awayTeam;
+            defendingTeam = &homeTeam;
+        }
+        
+        cout << "\n   --- [ATTACKING PHASE: FW vs DF] ---" << endl;
+        Player attackerFW = getRandomPlayer(*attackingTeam, "FW");
+        Player defenderDF = getRandomPlayer(*defendingTeam, "DF");
+        
+        cout << "   " << attackingTeam->teamName << " goes on the offensive! " << attackerFW.name << " (FW, Stat:" << attackerFW.stat << ") "
+             << "takes on " << defenderDF.name << " (DF, Stat:" << defenderDF.stat << ")!" << endl;
+             
+        if (duel1v1(attackerFW, defenderDF, *attackingTeam, *defendingTeam)) {
+            cout << "   => BRILLIANT SKILL! " << attackerFW.name << " dribbles past the defender and enters the penalty box!" << endl;
+            
+            cout << "\n      --- [ONE-ON-ONE: FW vs GK] ---" << endl;
+            Player defenderGK = getRandomPlayer(*defendingTeam, "GK");
+            
+            cout << "      It's just " << attackerFW.name << " against the goalkeeper, " << defenderGK.name << " (GK, Stat:" << defenderGK.stat << ")!" << endl;
+            
+            if (duel1v1(attackerFW, defenderGK, *attackingTeam, *defendingTeam)) {
+                cout << "      => GOAL!!! What a fantastic finish by " << attackerFW.name << "!" << endl;
+                cout << "\n==========================================" << endl;
+                cout << "         WINNER IS " << attackingTeam->teamName << " !!!" << endl;
+                cout << "==========================================\n" << endl;
+                matchEnded = true; 
+            } else {
+                cout << "      => SAVED! " << defenderGK.name << " makes a crucial save! The ball is cleared back to the midfield..." << endl;
+            }
+        } else {
+            cout << "      => TACKLED! Great defending by " << defenderDF.name << "! The ball is cleared back to the midfield..." << endl;
+        }
+    }
 }
